376a8f0ef37c930691e1aea8948ba7dc3b8d7184
kenji <silasotsuka@gmail.com>
2023-03-31 13:00:47 -0300

    Alteração Q6

--- Q_python/Q6.py
+++ Q_python/Q6.py
@@ -3,20 +3,13 @@
 # dentro dele com quebra de linhas por informação
 
 # Abrir o arquivo e imprimir o conteúdo na tela
-with open("arquivo_texto.txt", "r") as arquivo:
+'''
+arq = open('arquivoq6.txt', 'r')
+print(arq)
+'''
+
+with open("Q_python/arquivoq6.txt", "r") as arquivo:
     conteudo = arquivo.read()
     print(conteudo)
 
-# Criar um arquivo e escrever as informações
-nome = "Silas Kenji"
-idade = 20
-ano_inicio_curso = 2023
-
-with open("arquivo_texto.txt", "w") as arquivo:
-    arquivo.write(nome + "\n")
-    arquivo.write(str(idade) + "\n")
-    arquivo.write(str(ano_inicio_curso) + "\n")
-    
-#obs: essa aqui realmente me pegou de jeito, por mais que não seja algo complexo utiliza algumas funções do python com as quais n estou mto acostumado
-#ent acabei puxando uma ajudinha do stackoverflow, ainda vou reestruturar ela da minha maneira quando estiver com mais tempo     
     
